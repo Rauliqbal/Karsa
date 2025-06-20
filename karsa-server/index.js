e73082618f3b17558987ec07e789ab0ed178ca60
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2025-06-20 21:48:59 +0700

    chore: change typescript to javascript

---
+++ karsa-server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// MIDDLEWARE
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// ROUTES
+
+app.listen(port, () =>
+  console.log("[🔥INFO]: Server is running on http://localhost:%s", port)
+);
