6ed4abc36fb91188b947ed8917062d3bec7cb3a4
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2025-06-24 16:03:41 +0700

    feat(update-cv): added update CV controller

--- karsa-server/src/controllers/certificate.controller.js
+++ karsa-server/src/controllers/certificate.controller.js
@@ -38,3 +38,81 @@ export const createCertificate = async (req, res) => {
     });
   }
 };
+
+// Get Certificate
+export const getCertificate = async (req, res) => {
+  try {
+    const cv = await prisma.cV.findUnique({
+      where: { userId: req.user.id },
+    });
+
+    const certificate = await prisma.certificate.findMany({
+      where: {
+        cvId: cv.id,
+      },
+    });
+    res.status(200).json({
+      success: true,
+      message: "Berhasil mendapatkan certificate",
+      data: certificate,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+
+// Update Certificate
+export const updateCertificate = async (req, res) => {
+  const { id } = req.params;
+  const { name, issuer, issuedAt, link } = req.body;
+  try {
+    const certificate = await prisma.certificate.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        cv: true,
+      },
+    });
+    if (!certificate)
+      res.status(404).json({
+        success: false,
+        message: "Certificate tidak ditemukan",
+      });
+
+    if (certificate.cv.userId !== req.user.id) {
+      return res.status(403).json({
+        success: false,
+        message: "Anda tidak berhak mengubah certificate ini",
+      });
+    }
+
+    const updated = await prisma.certificate.update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+        issuer,
+        issuedAt,
+        link,
+      },
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Berhasil memperbarui certificate",
+      data: updated,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
--- karsa-server/src/routes/certificate.route.js
+++ karsa-server/src/routes/certificate.route.js
@@ -2,10 +2,20 @@ import express from "express";
 import { authorization } from "../middlewares/authorization.js";
 import validate from "../middlewares/validate.js";
 import { certificateSchema } from "../schemas/certificate.schema.js";
-import { createCertificate } from "../controllers/certificate.controller.js";
+import {
+  createCertificate,
+  getCertificate,
+  updateCertificate,
+} from "../controllers/certificate.controller.js";
 
 const route = express.Router();
 
 route.post("/", authorization, validate(certificateSchema), createCertificate);
-
+route.get("/", authorization, getCertificate);
+route.put(
+  "/:id",
+  authorization,
+  validate(certificateSchema),
+  updateCertificate
+);
 export const certificateRoute = route;
