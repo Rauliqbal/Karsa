239dbe7a39fc8b11fc52f7e0a3dca7697405458b
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2025-06-24 08:57:49 +0700

    feat(getAll-cv): added controller getAllCv rest api

--- karsa-server/src/controllers/cv.controller.js
+++ karsa-server/src/controllers/cv.controller.js
@@ -28,3 +28,74 @@ export const createCv = async (req, res) => {
     });
   }
 };
+
+// Get all CV
+export const getALlCv = async (req, res) => {
+  try {
+    const cv = await prisma.cV.findMany({
+      where: {
+        userId: req.user.id,
+      },
+    });
+
+    if (!cv) {
+      return res.status(404).json({
+        success: false,
+        message: "CV tidak ditemukan",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Berhasil mendapatkan semua CV",
+      data: cv,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+
+// Get CV by ID
+export const getCvById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const cv = await prisma.cV.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!cv) {
+      return res.status(404).json({
+        success: false,
+        message: "CV tidak ditemukan",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Berhasil mendapatkan CV",
+      data: cv,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+
+// Delete CV
+export const deleteCv = async (req, res) => {
+  await prisma.cV.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+};
--- karsa-server/src/routes/cv.route.js
+++ karsa-server/src/routes/cv.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { authorization } from "../middlewares/authorization.js";
-import { createCv } from "../controllers/cv.controller.js";
+import { createCv, getALlCv, getCvById } from "../controllers/cv.controller.js";
 
 const route = express.Router();
 
 route.post("/", authorization, createCv);
+route.get("/", authorization, getALlCv);
+route.get("/:id", authorization, getCvById);
 
 export const cvRoute = route;
