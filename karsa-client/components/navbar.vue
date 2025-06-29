164754cc843199d2356a9b01f6bfb7a2d9f14766
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2025-06-30 05:50:27 +0700

    wip

--- karsa-client/components/navbar.vue
+++ karsa-client/components/navbar.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <nav class="bg-white py-4 absolute top-0 left-0 right-0">
+  <nav
+    class="bg-white py-4 fixed top-0 left-0 right-0 shadow-md shadow-black/10"
+  >
     <div class="container flex items-center justify-between">
-      <img class="h-14" src="~/assets/images/karsaCrop.png" alt="" />
+      <img
+        class="h-10"
+        src="~/assets/images/logo-karsa.svg"
+        alt="KarsaCV adalah platform pembuatan CV modern yang membantu siapa saja — dari pelajar, mahasiswa, hingga profesional."
+      />
 
       <div class="flex items-center gap-4">
         <NuxtLink to="/" class="ml-4">Home</NuxtLink>
